7fea48a2202b67d45a30015f2881fcc8d94244cd
sid zhao <Sid@WEBEZI-SIsid.zhao@webezi.com.auD>
2018-04-17 16:05:32 +0800

    send bulk handsets

--- src/Sid.SmsKit/ClickatellClient.cs
+++ src/Sid.SmsKit/ClickatellClient.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Sid.SmsKit.Data;
 
@@ -20,13 +22,22 @@ namespace Sid.SmsKit
 
         private readonly RESTCredentials _credentials;
 
+        private readonly int _maxHandsetsCount = 500;
+
+        private readonly int _bulkHandsetsInterval;
+
+        private readonly ILogger _logger;
+
         #endregion
 
         #region Constructor
-        public ClickatellClient(RESTCredentials credentials)
+        public ClickatellClient(RESTCredentials credentials, ILogger logger = null, int bulkHandsetsInterval = 5000)
         {
             //Sets the REST API credentials
             _credentials = credentials;
+
+            _bulkHandsetsInterval = bulkHandsetsInterval;
+            _logger = logger;
         }
 
         #endregion
@@ -34,6 +45,63 @@ namespace Sid.SmsKit
         #region Public Methods
 
         public async Task<SendMessageResponse> SendMessageAsync(SendMessageRequest request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
+            if (string.IsNullOrEmpty(request.Message))
+            {
+                throw new SmsKitException("SMS message can not be null.");
+            }
+
+            if (request.PhoneNumbers == null || !request.PhoneNumbers.Any())
+            {
+                throw new SmsKitException("Must provide at least one phone number.");
+            }
+
+            // If phone number more than 500, it needs to be sent multiple times.
+            if (request.PhoneNumbers.Length <= _maxHandsetsCount)
+            {
+                return await Send(request);
+            }
+            else
+            {
+                _logger?.LogInformation($"Since more than {_maxHandsetsCount} phone number, so send it separately. It will send 500 at a time.");
+
+                var messages = new List<Message>();
+                var phoneNumbers = request.PhoneNumbers.ToList();
+
+                while (phoneNumbers.Count > 0)
+                {
+                    var newRequest = new SendMessageRequest
+                    {
+                        Message = request.Message,
+                        PhoneNumbers = phoneNumbers.Take(_maxHandsetsCount).ToArray()
+                    };
+
+                    var response = await Send(newRequest);
+
+                    messages.AddRange(response.Messages);
+
+                    phoneNumbers.RemoveRange(0, phoneNumbers.Count< _maxHandsetsCount? phoneNumbers.Count:_maxHandsetsCount);
+
+                    await Task.Delay(_bulkHandsetsInterval);
+                }
+
+                return new SendMessageResponse
+                {
+                    Messages = messages.ToArray()
+                };
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private async Task<SendMessageResponse> Send(SendMessageRequest request)
         {
             using (var client = new HttpClient())
             {
@@ -46,6 +114,8 @@ namespace Sid.SmsKit
 
                 try
                 {
+                    _logger?.LogInformation($"Start to send sms. Message is {request.Message}. Phone numbers are {string.Join(",", request.PhoneNumbers)}");
+
                     var response = await client.SendAsync(GetHttpRequestMessage(SendMessageURL, HttpMethod.Post, new StringContent(requestJson)));
 
                     if (response.StatusCode != HttpStatusCode.Accepted && response.StatusCode != HttpStatusCode.OK)
@@ -60,23 +130,25 @@ namespace Sid.SmsKit
                         To = message.to
                     }).ToArray();
 
-                    return new SendMessageResponse
+                    var res = new SendMessageResponse
                     {
                         Result = result,
                         Success = true,
                         Messages = jsonMessages
                     };
+
+                    _logger?.LogInformation($"Send sms successful. Response is {JsonConvert.SerializeObject(res)}");
+
+                    return res;
                 }
                 catch (Exception ex)
                 {
+                    _logger?.LogError($"Send sms failed. Error is {ex.Message}");
+
                     throw new SmsKitException($"Request failed. {ex.Message}");
                 }
             }
         }
-
-        #endregion
-
-        #region Private Methods
 
         private HttpRequestMessage GetHttpRequestMessage(string url, HttpMethod method, HttpContent content = null)
         {
